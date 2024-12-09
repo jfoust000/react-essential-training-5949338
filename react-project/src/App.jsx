358a03a4475b061254da61aa963912f133ed8bca
JFoust000 <joel.foust@yahoo.com>
2024-12-09 12:53:49 -0500

    lists and ids

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,8 +1,40 @@
 import './App.css'
 
+function Header({name, year}) {
+  return (
+    <header>
+        <h1> {name}'s Kitchen</h1>
+        <p>Copyright {year}</p>
+    </header>
+  );
+}
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Veggies"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+function Main({dishes}) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+      <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
-    <h1>Hello React!</h1>
+    <div>
+      <Header name="Joel" year={new Date().getFullYear()}/>
+       <Main dishes={dishObjects}/>
+    </div>
   );
 }
 
